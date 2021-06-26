71f8d8db25ee1d531af5538f6cf33188389965c8
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-06-26 14:13:04 -0700

    styled display section, high score, and add obj in js

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+/*
+Need an object that the program can randomly iterate through to display the property and its contents.
+
+set an object that records all questions and and their array of possible answers --> obj = {question: [answer1, answer2, answer3, answer4]}
+
+to differentiate between right and wrong answers, we can have all correct answers be index of 1 for the object property
+*/
+
+var questAndAns = {
+    'What is the purpose of CSS': ['Styling of webpages', 'Structure of webpage', 'Define the content of webpages',  'Stores data to be accessed'],
+
+    'Question2': ['Correct Answer', 'WrongAnswer1', 'WrongAnswer2', 'WrongAnswer3'],
+
+    'Question3': ['Correct Answer', 'WrongAnswer1', 'WrongAnswer2', 'WrongAnswer3'], 
+
+    'Question4': ['Correct Answer', 'WrongAnswer1', 'WrongAnswer2', 'WrongAnswer3'], 
+    
+    'Question5': ['Correct Answer', 'WrongAnswer1', 'WrongAnswer2', 'WrongAnswer3'],  
+}
